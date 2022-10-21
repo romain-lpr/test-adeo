2d0f6af19694c4b8e606dfdef0d4438409814b9c
romain-lpr <romain@lapilulerouge.io>
2022-10-21 16:47:24 +0200

    feat: MaJ test with argument

--- test/countFilteredList.test.js
+++ test/countFilteredList.test.js
@@ -1,12 +1,40 @@
 const {count,filter} = require('../app');
 
-describe('filter test', () => {
+describe('count filtered list test', () => {
+    let originalArgv;
+    beforeEach(() => {
+        originalArgv = process.argv;
+        jest.resetModules();
+    });
+    afterEach(() => {
+        process.argv = originalArgv;
+        console.log.mockClear();
+    });
+    beforeAll(() => {
+        jest.spyOn(console, 'log').mockImplementation(() => {});
+    });
+    afterAll(() => {
+        console.log.mockRestore();
+    });
+    it('should show that arguments are missing', () => {
+        require('../app.js');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual('Arguments missing')
+    });
+    it('should list the possible arguments when a wrong argument is given', () => {
+        process.argv.push('error')
+        require('../app.js');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual('Wrong arguments, only accept argument : filter, --filter, count, --count.')
+    });
     it('should show the animals matching with the ry string pattern', () => {
-        const result = count(filter('ry'));
-        expect(result).toEqual([{"name":"Uzuzozne [1]","people":[{"name":"Lillie Abbott [1]","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi [1]","people":[{"name":"Anthony Bruno [1]","animals":[{"name":"Oryx"}]}]}]);
+        process.argv[2] = 'filter=ry'
+        process.argv[3] = 'count'
+        require('../app');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual([{"name":"Uzuzozne [1]","people":[{"name":"Lillie Abbott [1]","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi [1]","people":[{"name":"Anthony Bruno [1]","animals":[{"name":"Oryx"}]}]}]);
     });
     it('should show that nothing has been found when no animals or people match the string pattern', () => {
-        const result = count(filter('ryad'));
-        expect(result).toEqual('Nothing found');
+        process.argv[2] = 'filter=ryad'
+        process.argv[3] = 'count'
+        require('../app');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual('Nothing found');
     });
 });
--- test/filter.test.js
+++ test/filter.test.js
@@ -1,12 +1,36 @@
-const {filter} = require('../app');
-
 describe('filter test', () => {
+    let originalArgv;
+    beforeEach(() => {
+        originalArgv = process.argv;
+        jest.resetModules();
+    });
+    afterEach(() => {
+        process.argv = originalArgv;
+        console.log.mockClear();
+    });
+    beforeAll(() => {
+        jest.spyOn(console, 'log').mockImplementation(() => {});
+    });
+    afterAll(() => {
+        console.log.mockRestore();
+    });
+    it('should show that arguments are missing', () => {
+        require('../app.js');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual('Arguments missing')
+    });
+    it('should list the possible arguments when a wrong argument is given', () => {
+        process.argv.push('error')
+        require('../app.js');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual('Wrong arguments, only accept argument : filter, --filter, count, --count.')
+    });
     it('should show the animals matching with the ry string pattern', () => {
-        const result = filter('ry');
-        expect(result).toEqual([{"name":"Uzuzozne","people":[{"name":"Lillie Abbott","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi","people":[{"name":"Anthony Bruno","animals":[{"name":"Oryx"}]}]}]);
+        process.argv[2] = 'filter=ry';
+        require('../app');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual([{"name":"Uzuzozne","people":[{"name":"Lillie Abbott","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi","people":[{"name":"Anthony Bruno","animals":[{"name":"Oryx"}]}]}]);
     });
     it('should show that nothing has been found when no animals or people match the string pattern', () => {
-        const result = filter('ryad');
-        expect(result).toEqual('Nothing found');
+        process.argv[2] = 'filter=ryad';
+        require('../app');
+        expect(JSON.parse(console.log.mock.calls[0])).toEqual('Nothing found');
     });
 });
